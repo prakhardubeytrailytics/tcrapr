21c092f38afeba5b3c97343c412e2bc98e062b27
prakhardubeytrailytics <prakhar.d@trailytics.com>
2025-07-22 10:24:19 +0530

    first commit

--- tscraper/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start virtual display
-echo "🎬 Starting virtual display on :99"
-Xvfb :99 -screen 0 1920x1080x24 &
-
-# Run Python script
-echo "🚀 Running TikTok scraper..."
-python scrape_shop.py
